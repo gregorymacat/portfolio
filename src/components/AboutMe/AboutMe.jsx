141b5cf865cc5c047da842476e316ff4dd896961
gregorymacat <gregorymacat@gmail.com>
2023-07-08 02:43:28 -0500

    Fixed resizing for smaller windows

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import './AboutMe.css';
 import InfoContainer from '../InfoContainer/InfoContainer.jsx';
+// import resumePDF from '../../../public/MacatGregoryResume.pdf';
 
 function AboutMe() {
+  // const handleResumeDownload = () => {
+  //   fetch('MacatGregoryResume.pdf').then(response => {
+  //     response.blob().then(blob => {
+  //       const fileUrl = window.URL.createObjectURL(blob);
+  //       let anchorTag = document.createElement('a');
+  //       anchorTag.href = fileUrl;
+  //       anchorTag.download = 'SamplePDF.pdf';
+  //       anchorTag.click();
+  //     })
+  //   });
+  // }
+
   return (
     <div className="about-me-container">
       <div className="about-me-background">
         <div className="profile-photo-downloads-container">
           <img id="profile-photo" src="./assets/profilephotolinkedin.JPG" alt="Profile Picture"></img>
-          <button id="projects-button">See My Projects</button>
-          <button id="download-resume">Download Resume</button>
+          <a id="projects-button" href="https://github.com/gregorymacat" target="_blank">See My Projects</a>
+          <a id="download-resume" href="./MacatGregoryResume.pdf" target="_blank">View Resume</a>
         </div>
         <div className="info-container">
           <h1>Hello! I'm Gregory Macat</h1>
